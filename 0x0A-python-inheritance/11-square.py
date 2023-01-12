9446ed1337d5cab3a17b4281db47dfe4fc69620c
calebere <calebere@yahoo.com>
2023-01-12 05:12:21 +0000

    module create a class Square that inherits from a rectangle

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-"""This module create a class Square that
-inherits from Rectangle"""
-
-
+"""Square class Module"""
 Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """Represents a square"""
-
+    """Square class"""
     def __init__(self, size):
-        """Initializes a square"""
-
+        """Method for initializing a square"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """computes the area of the square"""
+        """Method that returns area of a square"""
         return self.__size ** 2
 
-    def __str__(self) -> str:
-        """string representation"""
+    def __str__(self):
+        """Method that returns a string"""
         return "[Square] {}/{}".format(self.__size, self.__size)
-
